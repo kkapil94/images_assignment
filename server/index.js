40dc7ecd741e220a6692048f0f314a6a7f7af94e
kapil <khatrikapil987@gmail.com>
2024-01-15 03:28:36 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from "express"
-import path from "path"
 import userRoute from "./routes/userRoute.js"
 import imageRoute from "./routes/imageRoute.js"
 import {connect}from "./utils/mongodb.js"
@@ -27,21 +26,6 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/v1/auth",userRoute)
 app.use("/api/v1/image",imageRoute)
-
-//-----------deployment------------
-const __dirname1 = path.resolve()
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname1,"../client/dist")));
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"..","client","dist","index.html"))
-  })
-}else{
-  app.get("/",(req,res)=>{
-    res.send("Api is running")
-  })
-}
-
-//-----------deployment------------
 
 app.use(singleUpload)
 app.use(isValidated)
